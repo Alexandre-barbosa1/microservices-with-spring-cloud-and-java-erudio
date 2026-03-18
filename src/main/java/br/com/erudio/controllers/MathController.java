232fd4d06e9ba112c5fb48c56042758c871dfcdc
Alexandre <josealexandre2005@outlook.com>
2026-03-18 09:39:07 -0300

    adicionando a classe Math

---
+++ src/main/java/br/com/erudio/controllers/MathController.java
@@ -0,0 +1,28 @@
+package br.com.erudio.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private boolean isNumeric(String str) {
+        if (str == null || str.isEmpty()) return false;
+        String number = str.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    private Double convertToDouble(String str) throws IllegalArgumentException {
+        if (str == null || str.isEmpty()) throw new IllegalArgumentException();
+        String number = str.replaceAll(",", ".");
+        return Double.parseDouble(number);
+    }
+}
